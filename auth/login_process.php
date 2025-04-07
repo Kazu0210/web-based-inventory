4bd7a701f0eaf747144d15278e0f204e4b2eab55
Kazu0210 <clffrdkyl@gmail.com>
2025-04-07 21:23:33 +0800

    `Added password verification functionality and included necessary files in login_process.php and hash_password.php`

--- auth/login_process.php
+++ auth/login_process.php
@@ -1,7 +1,13 @@
 <?php
+include("../includes/db.php");
+include_once('../includes/hash_password.php');
+
 if (isset($_POST['login'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
+    if (verify_password($username, $password, $conn)) {
+        echo "Login Success";
+    }
 }
 ?>
--- includes/hash_password.php
+++ includes/hash_password.php
@@ -1,7 +1,28 @@
 <?php
+include("db.php");
+
 function hash_password($raw_password) {
     $hashed_password = password_hash($raw_password, PASSWORD_DEFAULT);
     return $hashed_password;
+}
+
+function verify_password($username, $str_password, $conn) {
+    // get hashed password from db
+    $sql = "SELECT password FROM user WHERE username = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $stmt->store_result();
+
+    if ($stmt->num_rows > 0) {
+        // Username exists
+        $stmt->close(); // Close the statement before returning
+        return true;
+    } else {
+        // Username does not exist
+        $stmt->close(); // Close the statement before returning
+        return false;
+    }
 }
 
 ?>
